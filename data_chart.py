cb525e3cbc2700277a20c9807dcfbad514d581a3
sky-forest <120348588+sky-forest@users.noreply.github.com>
2022-12-21 17:33:31 +0900

    Add files via upload

--- data_chart.py
+++ data_chart.py
@@ -30,7 +30,7 @@ def run_heatmap_app():
         st.bar_chart(df_selected)
 
 
-    selected_list3 = st.multiselect('군/구별 월 가구소득 데이터 확인하기', column_list[6:])
+    selected_list3 = st.multiselect('군/구별 가구소득 데이터 확인하기', column_list[6:])
     df_selected = df[selected_list3]
     if len(selected_list3) >= 1:
 
@@ -38,10 +38,10 @@ def run_heatmap_app():
         st.bar_chart(df_selected)
 
 
-    st.subheader('근무시간과 월 가구소득을 라인차트로 비교하기')
+    st.subheader('근무시간과 가구소득을 라인차트로 비교하기')
 
     selected_list4 = st.selectbox('근무시간을 선택해주세요.', column_list[:6])
-    selected_list44 = st.selectbox('월 가구소득을 선택해주세요.', column_list[6:])
+    selected_list44 = st.selectbox('가구소득을 선택해주세요.', column_list[6:])
     df_selected = df[[selected_list4, selected_list44]]
         
     st.line_chart(df_selected)
