81cb0d6344bf677b3a41eac56c369dcbbbd60a1c
sky-forest <120348588+sky-forest@users.noreply.github.com>
2022-12-21 17:26:55 +0900

    Add files via upload

---
+++ new_project.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import altair as alt
+
+from main import run_home_app
+from data_table import run_maxmin_app
+from data_chart import run_heatmap_app
+
+def main():
+    
+    st.title('인천시 군구별 근로시간에 따른 월 가구소득 비교')
+
+    menu = ['메인화면', '데이터 표', '데이터 차트']
+    choice = st.sidebar.selectbox('메뉴', menu)
+
+    if choice == '메인화면':
+        run_home_app()
+    elif choice == '데이터 표' :
+        run_maxmin_app()
+    elif choice == '데이터 차트' :
+        run_heatmap_app()
+
+if __name__ == '__main__' :
+    main()
