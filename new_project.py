21e5652e526aaef422abd08f472a7affda724278
sky-forest <chqhfksp@gmail.com>
2022-12-26 08:22:47 +0900

    fix

--- new_project.py
+++ new_project.py
@@ -12,14 +12,14 @@ def main():
     
     st.title('인천시 군구별 근로시간에 따른 월 가구소득 비교')
 
-    menu = ['메인화면', '데이터 표', '데이터 차트']
+    menu = ['메인화면', '표 데이터', '데이터 분석']
     choice = st.sidebar.selectbox('메뉴', menu)
 
     if choice == '메인화면':
         run_home_app()
-    elif choice == '데이터 표' :
+    elif choice == '표 데이터' :
         run_maxmin_app()
-    elif choice == '데이터 차트' :
+    elif choice == '데이터 분석' :
         run_heatmap_app()
 
 if __name__ == '__main__' :
